ec23cdcb0470e67b45718b3d3034c1e8ac81cb44
oknsoy <oksoymka@gmail.com>
2018-12-23 21:32:05 +0100

    custom pin color

--- front/src/components/CustomMap.js
+++ front/src/components/CustomMap.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer, Circle } from 'react-leaflet';
+import Leaflet from 'leaflet'
 
 export default class CustomMap extends Component {
 
@@ -12,7 +13,7 @@ export default class CustomMap extends Component {
 
         let markers = this.props.stations.map(station => {
             return (
-                <Marker position={station.location.coordinates.reverse()}>
+                <Marker position={station.location.coordinates.reverse()} icon={this.customPin('#2196F3')}>
                     <Popup>{station.address}</Popup>
                 </Marker>
             );
@@ -23,29 +24,53 @@ export default class CustomMap extends Component {
                 style={{
                     height: "700px"
                 }}>
-                <Map center={this.props.position} zoom={13} style={{height: "700px"}}>
-                    
+                <Map center={this.props.position} zoom={12} style={{ height: "700px" }}>
+
                     <TileLayer
                         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     />
-                    
-                    <Marker position={this.props.position}>
+
+                    <Marker position={this.props.position} icon={this.customPin('#F44336')}>
                         <Popup>
                             Bonjour, ceci est votre position.
                         </Popup>
                     </Marker>
-                    
+
                     <Circle
                         center={this.props.position}
                         radius={5 * 1000}
                     />
 
                     {markers}
-                
+
                 </Map>
             </div>
         );
+    }
+
+    customPin(color) {
+        const markerHtmlStyles = `
+        background-color: ${color};
+        width: 2rem;
+        height: 2rem;
+        display: block;
+        left: -1.5rem;
+        top: -1.5rem;
+        position: relative;
+        border-radius: 2rem 2rem 0;
+        transform: rotate(45deg);
+        border: 2px solid #FFFFFF`
+
+        const customIcon = Leaflet.divIcon({
+            className: "my-custom-pin",
+            iconAnchor: [0, 24],
+            labelAnchor: [-6, 0],
+            popupAnchor: [0, -36],
+            html: `<span style="${markerHtmlStyles}" />`
+        })
+
+        return customIcon;
     }
 
 }
