a1c5c5ce4a115e356a219753891cfae71e1e7f11
oknsoy <oksoymka@gmail.com>
2018-12-23 18:56:00 +0100

    adding cicle zone on map

--- front/src/components/CustomMap.js
+++ front/src/components/CustomMap.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, Marker, Popup, TileLayer, Circle } from 'react-leaflet';
 
 export default class CustomMap extends Component {
 
@@ -23,20 +23,26 @@ export default class CustomMap extends Component {
                 style={{
                     height: "700px"
                 }}>
-                <Map center={this.props.position} zoom={13}
-                    style={{
-                        height: "700px"
-                    }}>
+                <Map center={this.props.position} zoom={13} style={{height: "700px"}}>
+                    
                     <TileLayer
                         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     />
+                    
                     <Marker position={this.props.position}>
                         <Popup>
                             Bonjour, ceci est votre position.
                         </Popup>
                     </Marker>
+                    
+                    <Circle
+                        center={this.props.position}
+                        radius={5 * 1000}
+                    />
+
                     {markers}
+                
                 </Map>
             </div>
         );
