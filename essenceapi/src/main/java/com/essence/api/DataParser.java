bdf4b74db8f5273288b66ce89b4284e10fc728f4
oknsoy <oksoymka@gmail.com>
2019-01-13 20:48:11 +0100

    launching data update from servlet

--- essenceapi/src/main/java/com/essence/api/DataParser.java
+++ essenceapi/src/main/java/com/essence/api/DataParser.java
@@ -8,6 +8,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.servlet.http.HttpServlet;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
@@ -17,71 +18,78 @@ import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-public class DataParser extends TimerTask {
+public class DataParser extends HttpServlet {
 
     @Override
-    public void run() {
+    public void init() {
+        new Timer().schedule(new UpdateData(), 0, 15 * 60 * 1000);
+    }
 
-        //public static void main (String[]args){
-        DatabaseRequest dtb = new DatabaseRequest();
-        try {
-            File file = new File(".\\data.zip");
-            FileUtils.copyURLToFile(new URL("https://donnees.roulez-eco.fr/opendata/instantane"), file);
+    public static class UpdateData extends TimerTask {
 
-            ZipFile zipFile = new ZipFile(file.getCanonicalPath());
-            ZipEntry xmlFIle = zipFile.entries().nextElement();
+        @Override
+        public void run() {
 
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document = documentBuilder.parse(zipFile.getInputStream(xmlFIle));
+            DatabaseRequest dtb = new DatabaseRequest();
+            try {
+                File file = new File(".\\data.zip");
+                FileUtils.copyURLToFile(new URL("https://donnees.roulez-eco.fr/opendata/instantane"), file);
 
-            NodeList stations = document.getElementsByTagName("pdv");
+                ZipFile zipFile = new ZipFile(file.getCanonicalPath());
+                ZipEntry xmlFIle = zipFile.entries().nextElement();
 
-            ArrayList<Object> stations1 = new ArrayList<>();
-            for (int i = 0; i < stations.getLength(); ++i) {
-                Node s = stations.item(i);
+                DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+                Document document = documentBuilder.parse(zipFile.getInputStream(xmlFIle));
 
-                NodeList nodeFuels = ((Element) s).getElementsByTagName("prix");
-                if (nodeFuels.getLength() > 0) {
+                NodeList stations = document.getElementsByTagName("pdv");
 
-                    Map<String, String> fuels = new HashMap<>();
-                    for (int j = 0; j < nodeFuels.getLength(); ++j) {
-                        Node f = nodeFuels.item(j);
-                        fuels.put(f.getAttributes().getNamedItem("nom").getNodeValue(), f.getAttributes().getNamedItem("valeur").getNodeValue());
+                ArrayList<Object> stations1 = new ArrayList<>();
+                for (int i = 0; i < stations.getLength(); ++i) {
+                    Node s = stations.item(i);
+
+                    NodeList nodeFuels = ((Element) s).getElementsByTagName("prix");
+                    if (nodeFuels.getLength() > 0) {
+
+                        Map<String, String> fuels = new HashMap<>();
+                        for (int j = 0; j < nodeFuels.getLength(); ++j) {
+                            Node f = nodeFuels.item(j);
+                            fuels.put(f.getAttributes().getNamedItem("nom").getNodeValue(), f.getAttributes().getNamedItem("valeur").getNodeValue());
+                        }
+
+                        int id = Integer.parseInt(s.getAttributes().getNamedItem("id").getNodeValue());
+
+                        String attr_latitude = s.getAttributes().getNamedItem("latitude").getNodeValue();
+                        String attr_longitude = s.getAttributes().getNamedItem("longitude").getNodeValue();
+                        double[] coordinates = new double[2];
+                        if (!attr_latitude.isEmpty()) {
+                            coordinates[0] = Double.parseDouble(attr_longitude) / 100000;
+                            coordinates[1] = Double.parseDouble(attr_latitude) / 100000;
+                        }
+                        Location location = new Location("Point", coordinates);
+
+                        int zipcode = Integer.parseInt(s.getAttributes().getNamedItem("cp").getNodeValue());
+                        String city = ((Element) s).getElementsByTagName("ville").item(0).getTextContent().toUpperCase();
+                        String address = ((Element) s).getElementsByTagName("adresse").item(0).getTextContent().toLowerCase();
+
+
+                        Station station = new Station(id, zipcode, location, city, address, fuels);
+                        stations1.add(station);
                     }
-
-                    int id = Integer.parseInt(s.getAttributes().getNamedItem("id").getNodeValue());
-
-                    String attr_latitude = s.getAttributes().getNamedItem("latitude").getNodeValue();
-                    String attr_longitude = s.getAttributes().getNamedItem("longitude").getNodeValue();
-                    double[] coordinates = new double[2];
-                    if (!attr_latitude.isEmpty()) {
-                        coordinates[0] = Double.parseDouble(attr_longitude) / 100000;
-                        coordinates[1] = Double.parseDouble(attr_latitude) / 100000;
-                    }
-                    Location location = new Location("Point", coordinates);
-
-                    int zipcode = Integer.parseInt(s.getAttributes().getNamedItem("cp").getNodeValue());
-                    String city = ((Element) s).getElementsByTagName("ville").item(0).getTextContent().toUpperCase();
-                    String address = ((Element) s).getElementsByTagName("adresse").item(0).getTextContent().toLowerCase();
-
-
-                    Station station = new Station(id, zipcode, location, city, address, fuels);
-                    stations1.add(station);
                 }
+
+                dtb.dropCollection("station");
+                dtb.addMany("station", stations1);
+                zipFile.close();
+                System.out.println(file.delete());
+
+
+                String currentDate = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss").format(new Date());
+                dtb.add("info", new org.bson.Document().append("date", currentDate));
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            dtb.dropCollection("station");
-            dtb.addMany("station", stations1);
-            zipFile.close();
-            System.out.println(file.delete());
-
-
-            String currentDate = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss").format(new Date());
-            dtb.add("info", new org.bson.Document().append("date", currentDate));
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
--- essenceapi/src/main/java/com/essence/api/Main.java
+++ essenceapi/src/main/java/com/essence/api/Main.java
@@ -4,7 +4,6 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 
 import javax.ws.rs.ApplicationPath;
-import java.util.Timer;
 
 @ApplicationPath("/api")
 public class Main extends ResourceConfig {
@@ -17,15 +16,11 @@ public class Main extends ResourceConfig {
      */
 
 
-    public DatabaseRequest dtb;
+    protected DatabaseRequest dtb;
 
     public Main() {
         property(ServerProperties.TRACING, "ALL");
         this.dtb = new DatabaseRequest();
-
-        //update data every 15 minutes
-        Timer timer = new Timer();
-        timer.schedule(new DataParser(), 0, 15 * 60 * 1000);
     }
 
 }
